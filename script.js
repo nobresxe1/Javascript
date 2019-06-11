f055a242461c075296c1cdc257d9e225d5c29da8
nobresxe1 <rgs190385@gmail.com>
2019-06-10 21:46:12 -0300

    variavel

--- script.js
+++ script.js
@@ -2,5 +2,17 @@ alert("Olá Mundo!!!")
 var a;
 a = "aqui fica o conteudo da variavel";
 
+// + adição 
+// - subtração
+// * multiplicação
+// / divisão 
+// ++ incremento
+// -- decremento
 
-console.log(a);
+
+var a = 10;
+var b = 15;
+
+var c = a * b;
+
+console.log(c);
